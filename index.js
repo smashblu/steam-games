9bfebf48f1de08d6f9443edae860004f5a9d4bd9
smashblu <smashblu@gmail.com>
2025-08-05 15:35:39 -0700

    Create GET route for publisherId

--- index.js
+++ index.js
@@ -69,9 +69,20 @@ app.get('/games', (req, res) => {
   res.send(dummyGames)
 })
 
-app.get('/games/:gameId', (req, res, next) => {
+app.get('/games/gameId=:gameId', (req, res) => {
   const gameNum = req.params['gameId']
   res.send(dummyGames[gameNum - 1])
+})
+
+app.get('/games/publisherId=:publisherId', (req, res) => {
+  const pubNum = req.params['publisherId']
+  const pubList = []
+  for (game of dummyGames) {
+    if (game['publisher']['publisherId'] === parseInt(pubNum)) {
+      pubList.push(game)
+    }
+  }
+  res.send(pubList)
 })
 
 app.listen(port, () => {
