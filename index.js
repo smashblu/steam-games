efae6d9cd63d90704b78d9aeda5803fb93307893
smashblu <smashblu@gmail.com>
2025-08-05 11:33:24 -0700

    Create dummy objects for games and publishers

--- index.js
+++ index.js
@@ -4,7 +4,51 @@ const port = 3000
 
 const dummyGames = [
   {
-    title: 'Mario'
+    gameId: 1
+    title: 'Super Mario Bros. 3'
+    rating: 'E'
+    releaseDate: '1990-02-12'
+    currentPrice: 59.99
+    publisher: dummyPublishers[0]
+  },
+  {
+    gameId: 2
+    title: 'Sonic the Hedgehog 2'
+    rating: 'E'
+    releaseDate: '1992-11-24'
+    currentPrice: 59.99
+    publisher: dummyPublishers[1]
+  },
+  {
+    gameId: 3
+    title: 'Mortal Kombat 4'
+    rating: 'M'
+    releaseDate: '1997-09-11'
+    currentPrice: 59.99
+    publisher: dummyPublishers[2]
+  },
+  {
+    gameId: 4
+    title: 'The Legend of Zelda: Ocarina of Time'
+    rating: 'E'
+    releaseDate: '1998-11-23'
+    currentPrice: 59.99
+    publisher: dummyPublishers[0]
+  }
+]
+
+const dummyPublishers = [
+  {
+    publisherId: 1,
+    name: 'Nintendo'
+  },
+  {
+    publisherId: 2,
+    name: 'Sega'
+  },
+  {
+    publisherId: 3,
+    name: 'Midway'
   }
 ]
 
