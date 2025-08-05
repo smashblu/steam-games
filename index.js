006755891f1938ffdee887756b726bb3a4e9292e
Luke <luke@smashimacpro.smash.local>
2025-08-04 21:50:08 -0700

    Create sample data

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const { createServer } = require('node:http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const dummyGames = [
+  {
+    title: 'Mario'
+  }
+]
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Earth');
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.post('/', (req, res) => {
+  res.send('Received POST')
+})
+
+app.get('/games', (req, res) => {
+  res.send(dummyGames)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
