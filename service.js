9e795363d0b40bc732c26c66c9474018e5875634
smashblu <smashblu@gmail.com>
2025-08-28 11:14:43 -0700

    Move data to database.js, move all service functions

--- service.js
+++ service.js
@@ -1,3 +1,5 @@
+const { dummyGames } = require('./database.js')
+
 const validateNum = num => {
   if (isNaN(num)) {
     return false
@@ -5,4 +7,51 @@ const validateNum = num => {
   return true
 }
 
-module.exports = { validateNum }
+const existsHelper = (objArr, target, val) => {
+  for (item of objArr) {
+    if (item[val] === target) {
+      return true
+    }
+  }
+  return false
+}
+
+const findProperty = (el, target) => {
+  return dummyGames.find((element) => 
+  element[el] === target
+)}
+
+const findPropertyIndex = (el, target) => {
+  return dummyGames.findIndex((element) => 
+  element[el] === target
+)}
+
+const updateProps = (target, changes) => {
+  for (const key of Object.keys(changes)) {
+    if (target.hasOwnProperty(key)) {
+    changes[key] = target[key]
+    }
+  }
+  return
+}
+
+const matchProp = (obj, prop, curr) => {
+  if (obj.hasOwnProperty(prop)) {
+    if (obj[prop] !== curr) {
+      return true
+    }
+  }
+  return false
+}
+
+const makeList = (target, prop, val) => {
+  const list = []
+  for (game of dummyGames) {
+    if (game[prop][val] === target) {
+      list.push(game)
+    }
+  }
+  return list
+}
+
+module.exports = { validateNum, existsHelper, findProperty, findPropertyIndex, updateProps, matchProp, makeList }
