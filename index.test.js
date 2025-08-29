7087299b4feaebb42853bcffedcd125062b22e95
smashblu <smashblu@gmail.com>
2025-08-29 13:15:43 -0700

    Install Jest and create sample test

---
+++ index.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest')
+const app = require('../steam-games')
+
+describe('Sample Test', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
+  })
+})
+
