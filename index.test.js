ec7b71127750d2c0ebca2e46022fa9acf955f79a
smashblu <smashblu@gmail.com>
2025-09-15 16:47:31 -0700

    Insert mock data for endpoint testing

--- index.test.js
+++ index.test.js
@@ -1,10 +1,28 @@
 const request = require('supertest')
 const app = require('./index')
+const { listGames } = require("./database")
+
+jest.mock('./database', () => ({
+  listGames: jest.fn()
+}))
+
+const testGameList = [
+  {
+    gameId: 1,
+    title: 'Some Game',
+    publisher: {
+      publisherId: 1,
+      name: 'Some Publisher'
+    }
+  }
+]
+listGames.mockReturnValue(testGameList)
 
 describe('Test the Games path', () => {
   test('Should respond 200 to GET request', () => {
     return request(app)
       .get('/games')
-      .expect(200);
+      .expect(200)
+      .expect(testGameList)
   });
 });
