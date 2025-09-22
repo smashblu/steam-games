0fcade1a28fb2acf37cb37aba9bb6d8c21939b58
smashblu <smashblu@gmail.com>
2025-09-22 10:22:56 -0700

    Add test for POST operation

--- index.test.js
+++ index.test.js
@@ -1,9 +1,14 @@
 const request = require('supertest')
 const app = require('./index')
 const { listGames } = require("./database")
+const { existsHelper } = require('./service')
 
 jest.mock('./database', () => ({
   listGames: jest.fn()
+}))
+
+jest.mock('./service', () => ({
+  existsHelper: jest.fn(),
 }))
 
 const testGameList = [
@@ -24,5 +29,23 @@ describe('Test the Games path', () => {
       .get('/games')
       .expect(200)
       .expect(testGameList)
+  });
+
+  test('Should respond 201 to POST request with new game', () => {
+    const newGame = {
+      gameId: 2,
+      title: 'Another Game'
+    }
+
+    existsHelper.mockReturnValue(false)
+
+    return request(app)
+      .post('/games')
+      .send(newGame)
+      .expect(201)
+      .expect('Creation successful')
+      .then(() => {
+        expect(addGame).toHaveBeenCalledWith(newGame)
+      })
   });
 });
