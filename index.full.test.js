03ea4583d2862bce4a8121f3ce9d3643b0d47069
smashblu <smashblu@gmail.com>
2025-10-13 15:37:32 -0700

    Add full test for POST endpoint

--- index.full.test.js
+++ index.full.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const { app, server} = require('./index')
 const { listGames, addGame, delGame } = require("./database")
-const { existsHelper, validateNum, findProperty, findPropertyIndex, makeList, matchProp, updateProps } = require('./service')
+const { existsHelper, validateNum, findPropertyIndex } = require('./service')
 
 jest.mock('./database', () => ({
   listGames: jest.fn(),
@@ -25,11 +25,24 @@ const testGameList = [
 ]
 listGames.mockReturnValue(testGameList)
 
-describe('Preflight test', () => {
-  it('Number should return true', () => {
-    const testFunc = () => {
-        return true
+describe('Test full POST operation for /games path', () => {
+  it('Should respond 201 to POST request with new game', () => {
+    const newGame = {
+      gameId: 2,
+      title: 'Another Game'
     }
-    expect(testFunc()).toBe(true)
+    
+  const game = newGame
+  const gameNum = newGame['gameId']
+
+  return request(app)
+    .post('/games')
+    .send(newGame)
+    .expect(201)
+    .expect('Creation successful')
+    .then(() => {
+      testGameList.push(newGame)
+      expect(testGameList[1]).toMatchObject(newGame)
+    })
   })
 })
