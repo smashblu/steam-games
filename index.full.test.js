f17c94e33538110b78259fe76373a41e66078c79
smashblu <smashblu@gmail.com>
2025-10-21 14:39:30 -0700

    Add pre and post test for DELETE operation

--- index.full.test.js
+++ index.full.test.js
@@ -33,6 +33,7 @@ listGames.mockReturnValue(testGameList)
 
 describe('Test full POST operation for /games path', () => {
   it('Should respond 201 to POST request with new game', () => {
+
     return request(app)
       .post('/games')
       .send(newGame)
@@ -47,12 +48,18 @@ describe('Test full POST operation for /games path', () => {
 describe('Test full DELETE operation for /games/gameId path', () => {
   it('Should respond 204 to DELETE request', () => {
 
+    const preTest = []
+    preTest.push(testGameList[0])
+    preTest.push(newGame)
+
     return request(app)
       .delete('/games/gameId=1')
       .expect(204)
       .then(() => {
         testGameList.splice(0, 1)
         expect(testGameList[0]).toMatchObject(newGame)
+        expect(preTest.length).toEqual(2)
+        expect(testGameList.length).toEqual(1)
       })
   })
 })
