293c27bff615fa26ac14119b8d8e51152eabc1ce
smashblu <smashblu@gmail.com>
2025-11-25 11:59:10 -0800

    Include new db with correct naming scheme

---
+++ migrations/steam_games_ver-2.sql
@@ -0,0 +1,144 @@
+-- MySQL dump 10.13  Distrib 9.5.0, for macos26.1 (arm64)
+--
+-- Host: localhost    Database: steam
+-- ------------------------------------------------------
+-- Server version	9.5.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '4109f2e2-c4d8-11f0-aad2-95993f727e21:1-22';
+
+--
+-- Table structure for table `games`
+--
+
+DROP TABLE IF EXISTS `games`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `games` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `releaseDate` date DEFAULT NULL,
+  `rating` enum('Rating Pending','Rating Pending - Likely Mature 17+','Everyone','Everyone 10+','Teen','Mature 17+','Adults Only 18+') NOT NULL,
+  `publisherId` int DEFAULT NULL,
+  `currentPrice` decimal(14,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `publisher_id` (`publisherId`),
+  CONSTRAINT `games_ibfk_1` FOREIGN KEY (`publisherId`) REFERENCES `publisher` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `games`
+--
+
+LOCK TABLES `games` WRITE;
+/*!40000 ALTER TABLE `games` DISABLE KEYS */;
+INSERT INTO `games` VALUES (1,'Super Mario Bros. 3','1990-02-12','Everyone',1,19.99),(2,'Sonic the Hedgehog 2','1992-11-24','Everyone',2,9.99),(3,'Mortal Kombat 4','1997-09-11','Mature 17+',3,19.99),(4,'The Legend of Zelda: Ocarina of Time','1998-11-23','Everyone',1,59.99),(5,'Metal Gear Solid 3: Snake Eater','2004-11-17','Mature 17+',4,69.99);
+/*!40000 ALTER TABLE `games` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `library_games`
+--
+
+DROP TABLE IF EXISTS `library_games`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `library_games` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `game_id` int DEFAULT NULL,
+  `library_id` int DEFAULT NULL,
+  `playtime` time DEFAULT NULL,
+  `cost` decimal(14,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `library_id` (`library_id`),
+  KEY `game_id` (`game_id`),
+  CONSTRAINT `library_games_ibfk_1` FOREIGN KEY (`library_id`) REFERENCES `steam_library` (`id`),
+  CONSTRAINT `library_games_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `library_games`
+--
+
+LOCK TABLES `library_games` WRITE;
+/*!40000 ALTER TABLE `library_games` DISABLE KEYS */;
+/*!40000 ALTER TABLE `library_games` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `publisher`
+--
+
+DROP TABLE IF EXISTS `publisher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `publisher` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `publisher`
+--
+
+LOCK TABLES `publisher` WRITE;
+/*!40000 ALTER TABLE `publisher` DISABLE KEYS */;
+INSERT INTO `publisher` VALUES (1,'Nintendo'),(2,'Sega'),(3,'Midway'),(4,'Konami');
+/*!40000 ALTER TABLE `publisher` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `steam_library`
+--
+
+DROP TABLE IF EXISTS `steam_library`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `steam_library` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `steam_library`
+--
+
+LOCK TABLES `steam_library` WRITE;
+/*!40000 ALTER TABLE `steam_library` DISABLE KEYS */;
+INSERT INTO `steam_library` VALUES (1,'smashblu');
+/*!40000 ALTER TABLE `steam_library` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-25 11:58:16
